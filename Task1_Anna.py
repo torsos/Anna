6c76eacbce3386057d5b374be7ecb5f4e657e371
Anna Dunlap <anna.dunlap32@gmail.com>
2018-01-11 17:05:59 -0500

    Completed script by swapping a and b values

--- Task1_Anna.py
+++ Task1_Anna.py
@@ -2,11 +2,16 @@
 a = 8.9
 print type(a)
 
-
 b = 90000000000
 print type(b)
 
 c = b / a
-
 print "{} divided by {} is {}".format(b, a, c)
+
+# Assign variable a to a new variable then swap variable a and b.
+d = a
+a = b
+b = d
+print a
+print b
 
