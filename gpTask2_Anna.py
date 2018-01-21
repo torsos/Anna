dba85cf30c844cc0e5171b5e25eb5b69ea377b86
Anna Dunlap <anna.dunlap32@gmail.com>
2018-01-21 14:17:47 -0500

    Adding Week 2 assignment

---
+++ gpTask2_Anna.py
@@ -0,0 +1,8 @@
+gist_list = ['Geographic', 'Information', 'Systems', 'T', 315]
+
+## Assign second element to a variable.
+gist_2 = gist_list[2]
+
+## Print statement for steps 2-7 in Task1 Unit 2 each on a new line.
+print '\n'+ gist_2, '\n' + gist_2[:4], '\n' + gist_2.upper(), '\n' + gist_2.replace("s", "N",1), '\n' + gist_2[-1], '\n' + gist_2[3:6], '\n' + str(gist_list[4]) + "Syst"
+
